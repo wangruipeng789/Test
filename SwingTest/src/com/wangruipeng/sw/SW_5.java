46118784e5ad7abaa293a4175c3cda0ed53a0f8f
wayhe_wangruipeng <wangruipeng789>
2015-10-26 23:14:57 +0800

    aa

---
+++ SwingTest/src/com/wangruipeng/sw/SW_5.java
@@ -0,0 +1,15 @@
+package com.wangruipeng.sw;
+
+import javax.swing.JPanel;
+
+/*
+ * ͼ�λ滭
+ * */
+public class SW_5 {
+  public static void main(String[] args){
+	  
+  }
+}
+class MyJpanel extends JPanel{
+	
+}
