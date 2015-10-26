2884d7fd3efc8568703f9d9a85bb76aabd4fabe2
Administrator <Administrator@XB-20150404IWIE>
2015-10-26 23:32:06 +0800

    23;32

--- SW/src/com/wangruipeng/sw/SW_5.java
+++ SW/src/com/wangruipeng/sw/SW_5.java
@@ -1,11 +1,12 @@
 package com.wangruipeng.sw;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /*
  * ͼ�λ滭
  * */
-public class SW_5 {
+public class SW_5 extends JFrame{
   public static void main(String[] args){
 	  
   }
